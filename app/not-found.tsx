71d816d3f077d6564dfd18efe08a1a0fcf8fe5c7
Pouya <pouyasamadnejad@gmail.com>
2025-06-07 20:37:52 +0330

    not found page modified

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Button, Result } from "antd";
 function notFound() {
-  return <div>notFound</div>;
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center  h-screen">
+      <Result
+        status="404"
+        title="404"
+        subTitle="صفحه مورد نطر وجودندارد"
+        extra={
+          <Button type="primary" href="/">
+            بازگشت به خانه
+          </Button>
+        }
+      />
+    </div>
+  );
 }
 
 export default notFound;
